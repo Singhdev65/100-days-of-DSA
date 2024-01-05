934b0b26457750f1cb9ad7e71a7e180a0fca7036
singhdev65 <web.prince65@gmail.com>
2024-01-05 23:57:38 +0530

    day-1 valid palindrome

--- DAY-1/palindrome.js
+++ DAY-1/palindrome.js
@@ -1,12 +1,25 @@
-function Palindrome(s){
-    const string = s.replace(/[^A-Z0-9]/ig, "").toLowerCase();
-    const reverseString = string.split("").reverse().join("");
+// function Palindrome(s) {
+//   const string = s.replace(/[^A-Z0-9]/gi, "").toLowerCase()
+//   const reverseString = string.split("").reverse().join("")
 
-    if(string !== reverseString)  return false;
+//   if (string !== reverseString) return false
 
-    return true
+//   return true
+// }
+
+/**WITHOUT using reverse function */
+
+function Palindrome(s) {
+  const string = s.replace(/[^A-Z0-9]/gi, "").toLowerCase()
+  const length = string.length
+
+  for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    if (string[i] != string[length - 1 - i]) {
+      return false
+    }
+  }
+  return true
 }
-
 
 console.log(Palindrome("")) //true
 console.log(Palindrome("race a car")) //false
