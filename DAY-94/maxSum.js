fe4d1a3e077f90c14e62add3d0bb5719811759c5
singhdev65 <web.prince65@gmail.com>
2024-04-07 18:31:45 +0530

    DAY-94, max sum

---
+++ DAY-94/maxSum.js
@@ -0,0 +1,40 @@
+/*
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+*/
+
+function maxSubArray(nums) {
+  let maxSum = -Infinity;
+
+  for (let start = 0; start < nums.length; start++) {
+    let currentSum = 0;
+    for (let end = start; end < nums.length; end++) {
+      currentSum += nums[end];
+      maxSum = Math.max(maxSum, currentSum);
+    }
+  }
+
+  return maxSum;
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
