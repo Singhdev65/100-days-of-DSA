cfccc200e758c31e405bd17118ac51b0f01ef52b
singhdev65 <web.prince65@gmail.com>
2024-04-20 23:03:16 +0530

    Revision, DAY-6

--- DAY-7/isomorphic.js
+++ DAY-7/isomorphic.js
@@ -1,31 +1,56 @@
-function isIsomorphic(s, t) {
-  if (s.length !== t.length) return false
+/*
+Given two strings s and t, determine if they are isomorphic. 
 
-  let objStoT = {}
-  let objTtoS = {} 
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself._ 
+
+ 
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+
+Leetcode: https://leetcode.com/problems/isomorphic-strings/
+
+*/
+
+function isIsomorphic(s, t) {
+  if (s.length !== t.length) return false;
+
+  let objStoT = {};
+  let objTtoS = {};
 
   for (let i = 0; i < s.length; i++) {
-    const charS = s[i]
-    const charT = t[i]
+    const charS = s[i];
+    const charT = t[i];
 
     if (!objStoT[charS]) {
-      objStoT[charS] = charT
+      objStoT[charS] = charT;
     } else {
       if (objStoT[charS] !== charT) {
-        return false
+        return false;
       }
     }
 
     if (!objTtoS[charT]) {
-      objTtoS[charT] = charS
+      objTtoS[charT] = charS;
     } else {
       if (objTtoS[charT] !== charS) {
-        return false
+        return false;
       }
     }
   }
 
-  return true
+  return true;
 }
 
-console.log(isIsomorphic("badc", "baba")) // false
+console.log(isIsomorphic("badc", "baba")); // false
--- README.md
+++ README.md
@@ -166,6 +166,42 @@ function addStrings(num1, num2) {
 console.log(addStrings("11", "123")); //134
 ```
 
+7. **[Isomorphic Strings](https://leetcode.com/problems/isomorphic-strings/) ?**
+
+```javascript
+function isIsomorphic(s, t) {
+  if (s.length !== t.length) return false;
+
+  let objStoT = {};
+  let objTtoS = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const charS = s[i];
+    const charT = t[i];
+
+    if (!objStoT[charS]) {
+      objStoT[charS] = charT;
+    } else {
+      if (objStoT[charS] !== charT) {
+        return false;
+      }
+    }
+
+    if (!objTtoS[charT]) {
+      objTtoS[charT] = charS;
+    } else {
+      if (objTtoS[charT] !== charS) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+console.log(isIsomorphic("badc", "baba")); // false
+```
+
 **[⬆ Back to Top](#start-solving-you-will-fall-in-❤️❤️)**
 
 ---
