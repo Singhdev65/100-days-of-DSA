cf31315938dedbccee5c3a3fbfd439b04d7888b3
singhdev65 <web.prince65@gmail.com>
2024-04-21 23:00:08 +0530

    Revision. Day-7

--- DAY-8/duplicateString.js
+++ DAY-8/duplicateString.js
@@ -1,20 +1,36 @@
+/*
+
+ Given a string S, the task is to print all the duplicate characters with their occurrences in the given string .
+
+Example:
+
+Input: S = “geeksforgeeks”
+Output:
+
+e, count = 4
+g, count = 2
+k, count = 2
+s, count = 2
+
+*/
+
 function duplicateString(s) {
-  let obj = {}
+  let obj = {};
   for (let char of s) {
     if (obj[char]) {
-      obj[char]++
+      obj[char]++;
     } else {
-      obj[char] = 1
+      obj[char] = 1;
     }
   }
   for (let x in obj) {
     if (obj[x] > 1) {
-      continue
+      continue;
     } else {
-      delete obj[x]
+      delete obj[x];
     }
   }
-  return obj
+  return obj;
 }
 
-console.log(duplicateString("geeksofgeeks"))
+console.log(duplicateString("geeksofgeeks"));
