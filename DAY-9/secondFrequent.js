65ee5a84902ec8a05bfd5952093acbb97aa360fe
singhdev65 <web.prince65@gmail.com>
2024-04-22 22:57:42 +0530

    Revision, Day-9

--- DAY-9/secondFrequent.js
+++ DAY-9/secondFrequent.js
@@ -1,30 +1,68 @@
-function secondFrequent(arr) {
-  const obj = {}
-  arr.forEach(el => {
-    if (obj[el]) {
-      obj[el]++
-    } else {
-      obj[el] = 1
-    }
-  })
+/*
 
-  let maxCount = 0
-  let secondMaxCount = 0
-  let result = null
+Given a sequence of strings, the task is to find out the second most repeated (or frequent) string in the given sequence. 
+
+Note: No two strings are the second most repeated, there will always be a single string.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {aaa, bbb, ccc, bbb, aaa, aaa}
+Output: bbb
+Explanation: "bbb" is the second most 
+occurring string with frequency 2.
+
+Example 2:
+
+Input: 
+N = 6
+arr[] = {geek, for, geek, for, geek, aaa}
+Output: for
+Explanation: "for" is the second most
+occurring string with frequency 2.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function secFrequent() which takes the string array arr[] and its size N as inputs and returns the second most frequent string in the array. If no such string exists, then return an empty string.
+
+
+Expected Time Complexity: O(N*max(|Si|).
+Expected Auxiliary Space: O(N*max(|Si|).
+
+
+Constraints:
+1<=N<=103
+
+Link: https://www.geeksforgeeks.org/print-all-the-duplicates-in-the-input-string/
+
+*/
+
+function secondFrequent(arr) {
+  const obj = {};
+  arr.forEach((el) => {
+    if (obj[el]) {
+      obj[el]++;
+    } else {
+      obj[el] = 1;
+    }
+  });
+
+  let maxCount = 0;
+  let secondMaxCount = 0;
+  let result = null;
   for (const key in obj) {
-    
     if (maxCount < obj[key]) {
-      secondMaxCount = maxCount
-      maxCount = obj[key]
-      result = key
+      secondMaxCount = maxCount;
+      maxCount = obj[key];
+      result = key;
     } else if (secondMaxCount < obj[key] && obj[key] !== maxCount) {
-      secondMaxCount = obj[key]
-      result = key
+      secondMaxCount = obj[key];
+      result = key;
     }
   }
 
-  return secondMaxCount > 0 ? result : ""
+  return secondMaxCount > 0 ? result : "";
 }
 
-console.log(secondFrequent(["aaa", "bbb", "ccc", "bbb", "aaa", "aaa"])) // bbb
-console.log(secondFrequent(["geek", "for", "geek", "for", "geek", "aaa"])) //for
+console.log(secondFrequent(["aaa", "bbb", "ccc", "bbb", "aaa", "aaa"])); // bbb
+console.log(secondFrequent(["geek", "for", "geek", "for", "geek", "aaa"])); //for
--- README.md
+++ README.md
@@ -202,6 +202,65 @@ function isIsomorphic(s, t) {
 console.log(isIsomorphic("badc", "baba")); // false
 ```
 
+8. **[Duplicate String](https://www.geeksforgeeks.org/print-all-the-duplicates-in-the-input-string/) ?**
+
+```javascript
+function duplicateString(s) {
+  let obj = {};
+  for (let char of s) {
+    if (obj[char]) {
+      obj[char]++;
+    } else {
+      obj[char] = 1;
+    }
+  }
+  for (let x in obj) {
+    if (obj[x] > 1) {
+      continue;
+    } else {
+      delete obj[x];
+    }
+  }
+  return obj;
+}
+
+console.log(duplicateString("geeksofgeeks"));
+```
+
+9. **[Second repeated word sequence](https://www.geeksforgeeks.org/second-repeated-word-sequence/) ?**
+
+```javascript
+function secondFrequent(arr) {
+  const obj = {};
+  arr.forEach((el) => {
+    if (obj[el]) {
+      obj[el]++;
+    } else {
+      obj[el] = 1;
+    }
+  });
+
+  let maxCount = 0;
+  let secondMaxCount = 0;
+  let result = null;
+  for (const key in obj) {
+    if (maxCount < obj[key]) {
+      secondMaxCount = maxCount;
+      maxCount = obj[key];
+      result = key;
+    } else if (secondMaxCount < obj[key] && obj[key] !== maxCount) {
+      secondMaxCount = obj[key];
+      result = key;
+    }
+  }
+
+  return secondMaxCount > 0 ? result : "";
+}
+
+console.log(secondFrequent(["aaa", "bbb", "ccc", "bbb", "aaa", "aaa"])); // bbb
+console.log(secondFrequent(["geek", "for", "geek", "for", "geek", "aaa"])); //for
+```
+
 **[⬆ Back to Top](#start-solving-you-will-fall-in-❤️❤️)**
 
 ---
