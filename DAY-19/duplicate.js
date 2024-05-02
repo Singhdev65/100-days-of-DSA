da1721e7585e17db68312424270adefe1a350ed5
singhdev65 <web.prince65@gmail.com>
2024-05-02 22:34:51 +0530

    revision, Day-18

--- DAY-19/duplicate.js
+++ DAY-19/duplicate.js
@@ -1,19 +1,51 @@
+/*
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [3,3,3,3,3]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times
+
+Link: https://leetcode.com/problems/find-the-duplicate-number/
+*/
+
 function findDuplicate(nums) {
-  const obj = {}
-  nums.forEach(num => {
+  const obj = {};
+  nums.forEach((num) => {
     if (obj[num]) {
-      obj[num]++
+      obj[num]++;
     } else {
-      obj[num] = 1
+      obj[num] = 1;
     }
-  })
+  });
   for (let x in obj) {
     if (obj[x] > 1) {
-      return x
+      return x;
     }
   }
-  return null
+  return null;
 }
 
-console.log(findDuplicate([1, 3, 4, 2, 2])) //2
-console.log(findDuplicate([3, 1, 3, 4, 2])) //3
+console.log(findDuplicate([1, 3, 4, 2, 2])); //2
+console.log(findDuplicate([3, 1, 3, 4, 2])); //3
--- README.md
+++ README.md
@@ -479,6 +479,30 @@ console.log(runningSum([1, 1, 1, 1, 1])); // [1,2,3,4,5]
 console.log(runningSum([3, 1, 2, 10, 1])); // [3,4,6,16,17]
 ```
 
+19. **[Find Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/) ?**
+
+```javascript
+function findDuplicate(nums) {
+  const obj = {};
+  nums.forEach((num) => {
+    if (obj[num]) {
+      obj[num]++;
+    } else {
+      obj[num] = 1;
+    }
+  });
+  for (let x in obj) {
+    if (obj[x] > 1) {
+      return x;
+    }
+  }
+  return null;
+}
+
+console.log(findDuplicate([1, 3, 4, 2, 2])); //2
+console.log(findDuplicate([3, 1, 3, 4, 2])); //3
+```
+
 **[⬆ Back to Top](#start-solving-you-will-fall-in-❤️❤️)**
 
 ---
