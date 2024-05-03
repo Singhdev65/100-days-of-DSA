38b869df8a934fafbdc9a6bb2719adc8234bdc7a
singhdev65 <web.prince65@gmail.com>
2024-05-03 19:02:07 +0530

    Revision, DAY-19

--- DAY-20/colorSort.js
+++ DAY-20/colorSort.js
@@ -1,15 +1,37 @@
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Link: https://leetcode.com/problems/sort-colors/
+
+*/
+
 function sortColor(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] > arr[j]) {
-        temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
       }
     }
   }
-  return arr
+  return arr;
 }
 
-console.log(sortColor([2,0,2,1,1,0])); // [0,0,1,1,2,2]
-console.log(sortColor([2,0,1])); //[0,1,2]
+console.log(sortColor([2, 0, 2, 1, 1, 0])); // [0,0,1,1,2,2]
+console.log(sortColor([2, 0, 1])); //[0,1,2]
--- README.md
+++ README.md
@@ -503,6 +503,26 @@ console.log(findDuplicate([1, 3, 4, 2, 2])); //2
 console.log(findDuplicate([3, 1, 3, 4, 2])); //3
 ```
 
+20. **[Sort Color](https://leetcode.com/problems/sort-colors/) ?**
+
+```javascript
+function sortColor(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortColor([2, 0, 2, 1, 1, 0])); // [0,0,1,1,2,2]
+console.log(sortColor([2, 0, 1])); //[0,1,2]
+```
+
 **[⬆ Back to Top](#start-solving-you-will-fall-in-❤️❤️)**
 
 ---
